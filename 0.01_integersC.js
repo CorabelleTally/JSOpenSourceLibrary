4d3549bd425881cd901f1e8c66cd690c018bd65f
CorabelleTally <tallycorabelle@myocu.oak.edu>
2017-06-23 16:21:45 -0400

    Update 0.01_integersC.js

--- 0.01_integersC.js
+++ 0.01_integersC.js
@@ -7,3 +7,13 @@ console.log(5%2);
 console.log(14%3);
 console.log(51560%21);
 
+//random variables in console.log()
+console.log(17);
+console.log(2000);
+console.log(2017);
+console.log(0);
+
+console.log(9%3);
+console.log(73%2);
+console.log(94872%84);
+
