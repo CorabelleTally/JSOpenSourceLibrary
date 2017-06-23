4c242d79811fb24b45f2c31059fed5926055f026
CorabelleTally <tallycorabelle@myocu.oak.edu>
2017-06-23 16:20:46 -0400

    Update 0.01_integers.js

--- 0.01_integers.js
+++ 0.01_integers.js
@@ -16,3 +16,18 @@ console.log(10%5);
 console.log(10%4);
 
 console.log(7+7.1);
+
+//Console.log shows whatever you have displayed in it It can do all kinds of numbers. 
+console.log(3);
+console.log(3.3);
+console.log(3.3*3.3);
+
+// You are also able to do basic math opperations in the console.log()
+console.log(1+1);
+console.log(1-1);
+console.log(1*2);
+console.log(6/2);
+
+console.log(10%3);
+console.log(5%2);
+console.log(8+2.6);
