49e8b83c4d8bdde12ec15a6b235259122d13c5af
CorabelleTally <tallycorabelle@myocu.oak.edu>
2017-06-23 16:31:48 -0400

    Update 0.03_stringsC.js

--- 0.03_stringsC.js
+++ 0.03_stringsC.js
@@ -10,3 +10,17 @@ var model = "Mustang";
 var car= "2000 Ford Mustang"
 
 console.log("I drive a "+ car);
+
+// you can create your own equations and results. 
+
+var college = "Oakland City University";
+var graduationYear = 2019;
+
+console.log("I graduated from "+ college+ " in "+ graduationYear);
+
+var year = 4;
+var breed = "Quarter";
+
+var horse= "4 year old Quarter horse"
+
+console.log("I ride a "+ horse);
