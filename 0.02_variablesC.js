5483b7a003138f8b0d6f659e00ba8c38d7b7e7c9
CorabelleTally <tallycorabelle@myocu.oak.edu>
2017-06-23 16:26:13 -0400

    Update 0.02_variablesC.js

--- 0.02_variablesC.js
+++ 0.02_variablesC.js
@@ -11,3 +11,16 @@ var age=20;
 var lifespan=90;
 
 console.log(90-20);
+
+//you can create your own variables
+var b=23;
+var yo=99;
+var f=1;
+var z=42;
+var q=4;
+
+console.log(f+q);
+
+var enjoyment=8;
+var max=10;
+console.log(10-8);
