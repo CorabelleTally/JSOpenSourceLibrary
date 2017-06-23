49483cba9e526ff0932d370701670a718bc452c3
CorabelleTally <tallycorabelle@myocu.oak.edu>
2017-06-23 16:19:58 -0400

    Update 0.00_setup.js

--- 0.00_setup.js
+++ 0.00_setup.js
@@ -1,1 +1,7 @@
 console.log("Hello World");
+
+// Control+B in order to bring up the results. The console.log basically makes it do what you desire.
+// Up above makes the console say, Hello World. 
+// My version below says "How's it going?"
+
+console.log("How's it going?");
