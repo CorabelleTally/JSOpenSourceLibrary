09c6c00514bbc76973bb0c622402f1b78a843a32
CorabelleTally <tallycorabelle@myocu.oak.edu>
2017-06-23 16:24:20 -0400

    Update 0.02_variables.js

--- 0.02_variables.js
+++ 0.02_variables.js
@@ -32,3 +32,43 @@ console.log(ageInAugust);
 var eleven=11;
 console.log(eleven*eleven);
 console.log(eleven);
+
+console.log("-------")
+//Inserting my own variables
+
+var x = 3;
+console.log(x);
+
+var y = 2;
+var z = x+y;
+console.log(z);
+
+//Naming
+//Naming variables anything
+var age = 20
+var pizza = 1500;
+
+//Camel Casing
+//Writing out words with the first letters after the first word being capatalized
+var numberOfBooksIHaveWritten = 0;
+var myBirthYear = 1996;
+var newNumber = (z * numberOfBooksIHaveWritten)
+console.log(newNumber);
+
+//Shorthand Operators
+//Don't have to type it all out
+age -= 2;
+//same as
+age = age - 2;
+console.log(age);
+
+pizza%=634;
+//same as pizza=pizza%634
+console.log(pizza);
+
+//Using the var for a value interchangeable
+var ageInOctober=age +1;
+console.log(ageInOctober);
+var twenty=20;
+console.log(twenty*twenty);
+console.log(twenty);
