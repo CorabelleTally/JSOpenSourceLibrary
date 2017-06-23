b4d56cb651396407ac201ee39d2fe9fd911c53a9
CorabelleTally <tallycorabelle@myocu.oak.edu>
2017-06-23 16:27:02 -0400

    Update 0.03_strings.js

--- 0.03_strings.js
+++ 0.03_strings.js
@@ -42,3 +42,45 @@ console.log(highSchool.split(" "));
 //slice off everything before specified index
 //indicies of strings start at 0
 console.log(highSchool.slice(3));
+
+console.log("----------");
+//Strings are a group of characters
+//strings must go in quotes
+
+console.log("I really enjoy sitting in the coffee shop");
+
+var facebook="This evening was great!";
+console.log(facebook);
+var username= 'ctally';
+console.log(username);
+
+var password="123Hello";
+console.log(password);
+
+var birthCity= "Boulder";
+var birthState="Colorado";
+console.log(birthCity+birthState);
+var commaSpace = ", ";
+console.log(birthCity+commaSpace+birthState);
+console.log(birthCity+", "+birthState+3);
+
+//Mixing Strings and Integers
+var ageInOctober = 21;
+var highSchool= "Salem HS";
+var graduatedHS = 2015;
+
+console.log(highSchool+ commaSpace+ graduatedHS);
+console.log("My 'age' in October:",ageInOctober);
+console.log('My "age" in October:',ageInOctober);
+console.log("My \"age\" in October:",ageInOctober);
+
+//String Methods (functions)
+//You can edit the words with these functions
+
+console.log(highSchool.length);
+console.log(highSchool.toUpperCase());
+console.log(highSchool.toLowerCase());
+console.log(highSchool.split(" "));
+//slice off everything before specified index
+//indicies of strings start at 0
+console.log(highSchool.slice(1));
