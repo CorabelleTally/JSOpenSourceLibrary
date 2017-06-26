94040c0cfb89384c5e83e6293899c77a5b959d9e
CorabelleTally <tallycorabelle@myocu.oak.edu>
2017-06-26 09:21:12 -0400

    Update 0.05_conditionals_if_else.js

--- 0.05_conditionals_if_else.js
+++ 0.05_conditionals_if_else.js
@@ -26,3 +26,34 @@ if(elevatorStuckWhileWeAreOnIt && elevatorNumber === 13){
 }else{
 	console.log("Phew. That could have been trouble!");
 }
+
+console.log("----------")
+
+//If/Else statements 
+//Else statements give JS an alternative statement to execute
+
+var carOn = true;
+var carOff = true;
+var carBroken = false;
+var carBreaksDown = true;
+var interstateNumber = 65;
+
+if(carOn === true){
+	console.log("Going on a trip");
+}else{
+	console.log("Not let leaving");
+}
+
+if(carBroken === true){
+	console.log("We won't be going on vacation");
+}else{
+	console.log("We are headed out!");
+}
+
+//Combining variable types for conditionals
+
+if(carBreaksDown && interstateNumber === 65){
+	console.log("This sucks");
+}else{
+	console.log("Horray! We can keep driving!");
+}\
