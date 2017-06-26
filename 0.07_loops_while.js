837f7b87c1fce4e4b802d45eeb028690782f9a31
CorabelleTally <tallycorabelle@myocu.oak.edu>
2017-06-26 09:41:02 -0400

    Update 0.07_loops_while.js

--- 0.07_loops_while.js
+++ 0.07_loops_while.js
@@ -38,3 +38,46 @@ while(counter<100){
 	console.log("Halfway there");
 	}
 }
+
+console.log("------------");
+
+//Loops are used to do things a multiple times more easily
+//The while loop tells JS to repeat statements until a condition is false
+
+//Examples
+var score = 0;
+
+while(score<10){
+	score++;  //This is shorthand for saying(score = score+1)
+	console.log("Score is:" ,score)
+}
+
+// //Another example
+var effort = 0;
+while(effort<100){
+	effort+=10; ///shorthand for (effort=effort+10)
+	console.log("My effort is:" ,effort);
+}
+if(effort === 100){
+	console.log("That's a lot of effort!");
+}
+
+//challenge
+
+//define counter
+
+//while(condition){
+	//increment counter
+	//print counter
+	//check if 50
+		//if so, print message
+//}
+
+var counter = 0;
+while(counter<100){
+	counter+=10;
+	console.log("Counter is:",counter);
+	if(counter === 50){
+	console.log("Halfway there");
+	}
+}
