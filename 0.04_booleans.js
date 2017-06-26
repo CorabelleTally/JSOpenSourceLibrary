a25d75d2105d3675cc7b281f07a0ad2c3c65e663
CorabelleTally <tallycorabelle@myocu.oak.edu>
2017-06-26 09:10:28 -0400

    Update 0.04_booleans.js

--- 0.04_booleans.js
+++ 0.04_booleans.js
@@ -54,3 +54,63 @@ console.log("Two equals?", one === stringOne); //3 is very safe and compares typ
 console.log("&& operator:", 2===2 && 1===1 );
 console.log("|| operator:", 2===2 || 2===1);
 console.log("!= operator:", 2 != 1);
+
+console.log("---------")
+
+//Booleans
+//my edited version
+
+var tired= true;
+var awake= true;
+var hungry= true;
+var happy= true;
+var tiredOfNotSleepingInMyBed= true;
+var funnySometimes= true;
+
+console.log(corny);
+
+//comparisons
+//   ==   Equality
+//   ===   Strict Equality
+//    !=   Inequality
+//	  !==	Strict Inequality
+//	  >
+//     <
+//    >=
+//     <=   Greater than or equal to
+
+console.log(32>1);
+console.log(17<54);
+
+var test = 32>=3
+console.log(test);
+
+console.log("Thirty two is greater than three? " + (32>3));
+
+console.log("--------")
+
+//When checking for equality, you always want to use === over the == in JavaScript
+4=="4"; //true
+4==="4"; //false
+15===15    //true
+1+2===3  //true
+
+//inequality  --> doesn't equal
+"Friend" !== "Enemy"  //true
+3!== 3      //false
+
+//overview, difference between =,==,===
+var seven = 7;
+var stringOne = "7";
+console.log("Two equals?", one === stringOne); 
+console.log("Two equals?", one === stringOne); //3 is very safe and compares type and value
+
+//Logical Operators
+//Operators   Meaning    True expressions
+//   &&        and          4>0 && -2<0
+//   ||         or          4>0 || -2>0
+//   !          not			  !(5<0)
+
+console.log("&& operator:", 7===7 && 13===13);
+console.log("|| operator:", 7===7 || 5===2);
+console.log("!= operator:", 43 != 12);
