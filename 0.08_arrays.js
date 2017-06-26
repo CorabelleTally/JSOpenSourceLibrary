818bb848ae6ba4dead832875b3957b42c2be7cfc
CorabelleTally <tallycorabelle@myocu.oak.edu>
2017-06-26 09:44:10 -0400

    Update 0.08_arrays.js

--- 0.08_arrays.js
+++ 0.08_arrays.js
@@ -33,3 +33,41 @@ for(var i=0; i<books.length; i++){
 	console.log("The book is:", i);
 	console.log(books[i]);
 }
+
+console.log("--------");
+
+//Arrays are a collection of items 
+//We define them using []s
+
+var rainbowColors = ["Red","Orange","Yellow","Green","Blue","Indigo","Violet"];
+var raceWinners = [33,72,64];
+var myFavoriteThings = ["Pizza", 2, "Panic! At the Disco", true]; //mixed type array
+//Don't actually use a mixed array
+
+console.log(myFavoriteThings)
+
+//Arays have indexes
+
+var countries = ["England", "Spain", "Costa Rica"];
+//Indexes:         0        1          2
+
+console.log(countries[0]);
+
+console.log("------------")
+//Iterating over arrays
+
+var movies = ["Tangled", "Brave", "Beauty and the Beast", "Big Hero 6", "Frozen", "Sleeping Beauty"];
+
+//console.log(movies[0]);
+
+for(var i =0; i<movies.length; i++){
+	console.log("The index is:",i);
+	console.log(movies[i]);
+}
+
+var books = ["Time Travelor's Wife", "Harry Potter", "Percy Jackson", "The Princess Bride"];
+
+for(var i=0; i<books.length; i++){
+	console.log("The book is:", i);
+	console.log(books[i]);
+}
