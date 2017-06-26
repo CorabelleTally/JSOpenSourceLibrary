98d8f35d0f6cdd8ce948422715306c55940bc07a
CorabelleTally <tallycorabelle@myocu.oak.edu>
2017-06-26 09:37:18 -0400

    Update 0.07_loops_for.js

--- 0.07_loops_for.js
+++ 0.07_loops_for.js
@@ -32,3 +32,40 @@ for(var i=0; i<20; i++){
 //other loops->look up on own time
 //for in loops
 //do while loops
+
+console.log("----------")
+
+/For loops
+//For loops are similar to while loops in that they let us repeat statements
+//However they are more specialized
+
+//for(counter;condition;increment){
+//		Code to be run
+//}
+
+// for(var i = 1; i<50; i++){
+// 	console.log(i);
+// }
+
+// for(var i= 0; i<=100; i+=5){
+// 	console.log(i);
+// }
+
+// for(var i=3; i<40; i+=7){
+// 	console.log(i);
+// }
+
+//Break statement <- Covered in switch earlier
+//breaks are used to prematurely exit a loop
+
+for(var i=0; i<20; i++){
+	console.log("Testing:",i);
+	if(i===13){
+		console.log("Found 13!");
+		break;
+	}
+}
+
+//other loops->look up on own time
+//for in loops
+//do while loops
