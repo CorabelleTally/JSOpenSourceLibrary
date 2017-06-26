6b088f8d8db4357a94f03c360468fa28cd7c3173
CorabelleTally <tallycorabelle@myocu.oak.edu>
2017-06-26 09:31:52 -0400

    Update 0.05_conditionals_switch.js

--- 0.05_conditionals_switch.js
+++ 0.05_conditionals_switch.js
@@ -24,3 +24,33 @@ switch(marginOfSuperBowlWin){
 }
 
 console.log("Switch Result:", result); //console.log("Switch Result:" + result);
+
+
+ console.log("----------");
+
+ //Switch statements
+//Very similar to if statements
+
+//How it works:
+//The value is evaluated once
+//It is then compared wtih cases
+//If a matching case is found, executes code
+
+var goalsFinallyScored = 9;
+var result;
+
+switch(goalsFinallyScored){
+	case 1:
+		result = "That will be a good game";
+		break;
+	case 5:
+		result = "This is nerve racking";
+		break;
+	case 9:
+		result = "That was an easy win";
+		break;
+	default:
+		result = "I don't know. Let's see what happens";		
+}
+
+console.log("Switch Result:", result); //console.log("Switch Result:" + result);
